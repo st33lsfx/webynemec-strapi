6f0ad550730b351860dd23dc5bc21b7c8e904bcc
Ondřej Němec <ondrej.nemec@unifer.cz>
2026-02-15 22:49:40 +0100

    Refactor server startup process and update package.json commands
    - Changed the start command in package.json to utilize Strapi's built-in start command instead of node server.js.
    - Removed the server.js file, streamlining the project structure by relying on Strapi's internal server management.

--- server.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const path = require('path');
-const { createStrapi } = require('@strapi/core');
-
-const appDir = path.resolve(__dirname);
-const distDir = path.resolve(__dirname, 'dist');
-
-createStrapi({ appDir, distDir }).start();
